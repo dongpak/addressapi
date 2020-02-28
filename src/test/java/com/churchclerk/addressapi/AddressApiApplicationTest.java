cc33a504d1aa6822d560d1548c668cf7edc3ed35
Dong Pak <dong.pak@outlook.com>
2020-02-28 16:58:56 -0500

    Added more tests and ordered the test executions.

--- src/test/java/com/churchclerk/addressapi/AddressApiApplicationTest.java
+++ src/test/java/com/churchclerk/addressapi/AddressApiApplicationTest.java
@@ -6,7 +6,10 @@ import com.churchclerk.addressapi.model.Address;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -16,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import java.util.UUID;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AddressApiApplicationTest {
 
 	@Autowired
@@ -28,20 +32,16 @@ public class AddressApiApplicationTest {
 	private TestRestTemplate restTemplate;
 
 	@Test
+	@Order(0)
 	public void contexLoads() throws Exception {
 		Assertions.assertThat(api).isNotNull();
 	}
 
 	@Test
+	@Order(1)
 	public void testGetResources() throws Exception {
 
-		String response = restTemplate.getForObject(createUrl(), String.class);
-
-		Assertions.assertThat(response).isNotNull();
-
-		JsonObject	page = new Gson().fromJson(response, JsonObject.class);
-
-		Assertions.assertThat(page.get("numberOfElements").getAsLong()).isEqualTo(0L);
+		getResourcesAndCheck(createUrl(), 0L);
 	}
 
 	private String createUrl() {
@@ -62,6 +62,7 @@ public class AddressApiApplicationTest {
 	}
 
 	@Test
+	@Order(2)
 	public void testPostResource() throws Exception {
 
 		Address	testdata = createAddress(1000);
@@ -75,7 +76,7 @@ public class AddressApiApplicationTest {
 		address.setStreet(number + " Test Street");
 		address.setCity("Test City");
 		address.setState("Test State");
-		address.setZip("0" + number);
+		address.setZip("" + number);
 		address.setCountry("Test Country");
 		address.setActive(true);
 
@@ -109,6 +110,7 @@ public class AddressApiApplicationTest {
 	}
 
 	@Test
+	@Order(3)
 	public void testGetResource() throws Exception {
 
 		Address	testdata 	= createAddress(1001);
@@ -122,6 +124,7 @@ public class AddressApiApplicationTest {
 	}
 
 	@Test
+	@Order(4)
 	public void testUpdateResource() throws Exception {
 
 		Address	testdata 	= createAddress(1002);
@@ -141,6 +144,7 @@ public class AddressApiApplicationTest {
 	}
 
 	@Test
+	@Order(5)
 	public void testDeleteResource() throws Exception {
 
 		Address	testdata 	= createAddress(1003);
@@ -153,4 +157,58 @@ public class AddressApiApplicationTest {
 		Assertions.assertThat(actual.getStatusCode().value()).isEqualTo(500);
 	}
 
+	@Test
+	@Order(6)
+	public void testGetResourcesPagination() throws Exception {
+
+		createResourceAndCheck(createAddress(1004));
+		createResourceAndCheck(createAddress(1005));
+
+		getResourcesAndCheck(createPaginationUrl(0, 1), 1L);
+		getResourcesAndCheck(createPaginationUrl(0, 2), 2L);
+		getResourcesAndCheck(createPaginationUrl(1, 1), 1L);
+		getResourcesAndCheck(createPaginationUrl(9, 5), 0L);
+	}
+
+	private void getResourcesAndCheck(String url, long count) {
+		String response = restTemplate.getForObject(url, String.class);
+
+		Assertions.assertThat(response).isNotNull();
+
+		JsonObject page = new Gson().fromJson(response, JsonObject.class);
+
+		Assertions.assertThat(page.get("numberOfElements").getAsLong()).isEqualTo(count);
+	}
+
+	private String createPaginationUrl(int page, int size) {
+		StringBuffer buffer = new StringBuffer(createUrl());
+
+		buffer.append("?page=").append(page);
+		buffer.append("&size=").append(size);
+
+		return buffer.toString();
+	}
+
+	@Test
+	@Order(7)
+	public void testGetResourcesFilter() throws Exception {
+
+		createResourceAndCheck(createAddress(1006));
+		createResourceAndCheck(createAddress(1007));
+
+		getResourcesAndCheck(createFilterUrl("street", "1006%"), 1L);
+		getResourcesAndCheck(createFilterUrl("zip", "1007"), 1L);
+		getResourcesAndCheck(createFilterUrl("state", "Test%"), 6L);
+	}
+
+	private String createFilterUrl(String field, String value) {
+		StringBuffer buffer = new StringBuffer(createUrl());
+
+		buffer.append("?");
+		buffer.append(field);
+		buffer.append("=");
+		buffer.append(value);
+
+		return buffer.toString();
+	}
 }
